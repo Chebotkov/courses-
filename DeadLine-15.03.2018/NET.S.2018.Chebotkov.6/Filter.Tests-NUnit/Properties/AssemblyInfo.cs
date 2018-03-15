403d9f5a33102d7ca5ecaf7a8f7e683193978f4b
Chebotkov <35760739+Chebotkov@users.noreply.github.com>
2018-03-15 19:59:32 +0300

    Add files via upload

---
+++ DeadLine-15.03.2018/NET.S.2018.Chebotkov.6/Filter.Tests-NUnit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Filter.Tests-NUnit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Filter.Tests-NUnit")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("317b831d-db9b-4f1e-8e21-099ce00935cb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
