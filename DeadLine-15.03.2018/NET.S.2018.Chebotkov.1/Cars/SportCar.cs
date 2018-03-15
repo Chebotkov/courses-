1cbdd29b097cfe4f64e28361e0da69ac4ceb2d15
Chebotkov <35760739+Chebotkov@users.noreply.github.com>
2018-03-15 20:50:37 +0300

    Add files via upload

---
+++ DeadLine-15.03.2018/NET.S.2018.Chebotkov.1/Cars/SportCar.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cars
+{
+    public class SportCar
+    {
+        public void SportCarInfo(string information)
+        {
+            Console.WriteLine("Information about sport-car: {0}", information);
+        }
+    }
+}
---
+++ DeadLine-15.03.2018/NET.S.2018.Chebotkov.1/Cars/auto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cars
+{
+    public class Auto
+    {
+        public void AutoInfo(string information)
+        {
+            Console.WriteLine("Information about car: {0}", information);
+        }
+    }
+}
